322fb7f314fc7944e0b6eccfafd1f364ea761f5f
Huang Lianghua <withchris@126.com>
2019-12-20 22:37:49 +0800

    Update FRN module to make it readily usable.

--- frn.py
+++ frn.py
@@ -1,34 +1,37 @@
 import torch
 import torch.nn as nn
 
+
 class FilterResponseNormalization(nn.Module):
-    def __init__(self, beta, gamma, tau, eps=1e-6):
+    
+    def __init__(self, num_features, eps=1e-6, learnable_eps=False):
         """
         Input Variables:
         ----------------
-            beta, gamma, tau: Variables of shape [1, C, 1, 1].
+            num_features: A integer indicating the number of input feature dimensions.
             eps: A scalar constant or learnable variable.
+            learnable_eps: A bool value indicating whether the eps is learnable.
         """
-
-        super(FilterResponseNormalization, self).__init__()
-        self.beta = beta
-        self.gamma = gamma
-        self.tau = tau
-        self.eps = torch.Tensor([eps])
-
+        super(FRN, self).__init__()
+        self.eps = nn.Parameter(torch.Tensor([eps]))
+        if not learnable_eps:
+            self.eps.requires_grad_(False)
+        self.gamma = nn.Parameter(torch.Tensor(1, num_features, 1, 1))
+        self.beta = nn.Parameter(torch.Tensor(1, num_features, 1, 1))
+        self.tau = nn.Parameter(torch.Tensor(1, num_features, 1, 1))
+        self.reset_parameters()
+    
     def forward(self, x):
         """
         Input Variables:
         ----------------
             x: Input tensor of shape [NxCxHxW]
         """
-
-        n, c, h, w = x.shape
-        assert (self.gamma.shape[1], self.beta.shape[1], self.tau.shape[1]) == (c, c, c)
-
-        # Compute the mean norm of activations per channel
-        nu2 = torch.mean(x.pow(2), (2,3), keepdims=True)
-        # Perform FRN
+        nu2 = torch.pow(x, 2).mean(dim=(2, 3), keepdim=True)
         x = x * torch.rsqrt(nu2 + torch.abs(self.eps))
-        # Return after applying the Offset-ReLU non-linearity
-        return torch.max(self.gamma*x + self.beta, self.tau)
+        return torch.max(self.gamma * x + self.beta, self.tau)
+
+    def reset_parameters(self):
+        nn.init.ones_(self.gamma)
+        nn.init.zeros_(self.beta)
+        nn.init.zeros_(self.tau)
